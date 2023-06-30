9251d0e0a38f684617bd0d6f4f7eb8d4ea3007e3
Dsw50235 <137617930+Dsw50235@users.noreply.github.com>
2023-06-30 23:37:45 +0200

    Add files via upload

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-if [[ $1 == "--logs" ]]; then
-    for ((i=1; i<=100; i++))
+if [[ $1 == "--data" ]]; then
+    current_date=$(date +%Y-%m-%d)
+    echo "Aktualna data: $current_date"
+elif [[ $1 == "--logs" ]]; then
+    if [[ -z $2 ]]; then
+        num_logs=100
+    else
+        num_logs=$2
+    fi
+
+    for ((i=1; i<=num_logs; i++))
     do
         filename="log${i}.txt"
         echo "Nazwa pliku: ${filename}" > "${filename}"
@@ -9,5 +18,5 @@ if [[ $1 == "--logs" ]]; then
         echo "Data utworzenia: $(date)" >> "${filename}"
     done
 else
-    echo "Podaj argument --logs, aby uruchomić skrypt i utworzyć 100 plików."
+    echo "Podaj argument --data lub --logs [liczba], aby uruchomić skrypt."
 fi
